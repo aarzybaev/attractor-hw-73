1c9f897106106e84074cbfd44b9bec64a70a21c9
Askhad Arzybaev <aarzybaev@gmail.com>
2024-03-13 12:33:53 +0600

    completed task-1

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 const express = require('express');
-const vigenere = require()
+const Vigenere = require('caesar-salad');
+const app = express();
+const port = 8000;
+
+app.get('/', (request, response) => {
+    response.send('We are live on ' + port);
+});
+app.get('/:echo', (request, response) => {
+    response.send('Echo: ' + request.params.echo);
+});
+
+app.listen(port, () => {
+   console.log('We are live on ' + port);
+});
