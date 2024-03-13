99d4384d3e2c0dcfba2ef8e18cf462a79e8c5cfb
Askhad Arzybaev <aarzybaev@gmail.com>
2024-03-13 12:39:39 +0600

    done an encode feature in task-2

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const Vigenere = require('caesar-salad');
+const Vigenere = require('caesar-salad').Vigenere;
 const app = express();
 const port = 8000;
+const password = 'attractor';
 
 app.get('/', (request, response) => {
     response.send('We are live on ' + port);
@@ -9,6 +10,13 @@ app.get('/', (request, response) => {
 app.get('/:echo', (request, response) => {
     response.send('Echo: ' + request.params.echo);
 });
+
+app.get('/encode/:message', (request, response) => {
+   const message = request.params.message;
+   const encodedMessage = Vigenere.Cipher(password).crypt(message);
+   response.send(encodedMessage);
+});
+
 
 app.listen(port, () => {
    console.log('We are live on ' + port);
